fddd023a45cf88f06ffd0346ecd91453cadd129a
Abstynent <lukjur94@gmail.com>
2023-06-17 15:34:10 +0200

    header styling added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,22 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import React from 'react';
+import './Header.css';
 const title = "wookashPortfolio";
 
 function Header() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+    <Navbar collapseOnSelect expand="lg" className="header-bg-custom">
       <Container>
-        <Navbar.Brand href="#home">{title}</Navbar.Brand>
+        <Navbar.Brand className='header-item'>{title}</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">About Me</Nav.Link>
-            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-            <Nav.Link href="#resume">Resume</Nav.Link>
+            <Nav.Link className='header-item dropdown' href="#home">About Me</Nav.Link>
+            <Nav.Link className='header-item dropdown'  href="#portfolio">Portfolio</Nav.Link>
+            <Nav.Link className='header-item dropdown'  href="#contact">Contact</Nav.Link>
+            <Nav.Link className='header-item dropdown'  href="#resume">Resume</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
