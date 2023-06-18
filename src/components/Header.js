8a87f28652db8bffad5bbcd272e6e2544de909e7
Abstynent <lukjur94@gmail.com>
2023-06-18 14:15:15 +0200

    onClick function added to each link

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,18 +6,43 @@ import '../css/Header.css';
 
 const title = "wookashPortfolio";
 
-function Header() {
+function Header({ currentPage, handlePageChange }) {
   return (
     <Navbar collapseOnSelect expand="lg" className="header-bg-custom">
       <Container>
-        <Navbar.Brand className='header-item'>{title}</Navbar.Brand>
+        <Navbar.Brand className='header-item' href="/">{title}</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link className='header-item dropdown' href="#home">About Me</Nav.Link>
-            <Nav.Link className='header-item dropdown'  href="#portfolio">Portfolio</Nav.Link>
-            <Nav.Link className='header-item dropdown'  href="#contact">Contact</Nav.Link>
-            <Nav.Link className='header-item dropdown'  href="#resume">Resume</Nav.Link>
+            <Nav.Link 
+            className='header-item dropdown hover-underline-animation' 
+            href="#home"
+            onClick={() => handlePageChange('About')}
+            >
+              About Me
+            </Nav.Link>
+
+            <Nav.Link 
+            className='header-item dropdown hover-underline-animation' 
+            href="#portfolio"
+            onClick={() => handlePageChange('Portfolio')}
+            >
+              Portfolio
+            </Nav.Link>
+            <Nav.Link 
+            className='header-item dropdown hover-underline-animation' 
+            href="#contact"
+            onClick={() => handlePageChange('Contact')}
+            >
+              Contact
+            </Nav.Link>
+            <Nav.Link 
+            className='header-item dropdown hover-underline-animation' 
+            href="#resume"
+            onClick={() => handlePageChange('Resume')}
+            >
+              Resume
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
