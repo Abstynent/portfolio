292ec4f022748982d94cf257225cd2d090d959b2
Abstynent <lukjur94@gmail.com>
2023-06-18 14:14:39 +0200

    logic added to render selected page by user from the navbar

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/Body.css';
 
+import About from './About';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+import Header from './Header';
+import Resume from './Resume';
+
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
 function Body() {
+    const [currentPage, setCurrentPage] = useState('Home');
+
+    const renderPage = () => {
+        if(currentPage === 'Home') return (
+            <Container fluid className="h-100 d-flex align-items-center justify-content-center middle">
+            <Row>
+              <Col className='text-center'>
+                <h1><span className='greeting'>Hi!</span> I am Lukasz Jurkiewicz</h1>
+                <h2>Full Stack Developer</h2>
+                <Button href='#about-me'>About me</Button>
+              </Col>
+            </Row>
+        </Container>);
+        if(currentPage === 'About') return <About />;
+        if(currentPage === 'Portfolio') return <Portfolio />;
+        if(currentPage === 'Contact') return <Contact />;
+        if(currentPage === 'Resume') return <Resume />
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
     return (
-        <div>something</div>
-    )
+        <div>
+            <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+        </div>
+    );
 };
 
 export default Body;
