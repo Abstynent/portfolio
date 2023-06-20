6d575d2ac9e4908db0200cecc350166feafea70b
Abstynent <lukjur94@gmail.com>
2023-06-20 19:00:11 +0200

    typewriter effect added to the home page

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,19 +7,17 @@ import Contact from './Contact';
 import Header from './Header';
 import Resume from './Resume';
 
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function Body() {
     const [currentPage, setCurrentPage] = useState('Home');
-
     const renderPage = () => {
         if(currentPage === 'Home') return (
-            <Container fluid className="h-100 d-flex align-items-center justify-content-center middle">
+            <Container fluid className="bg-img h-100 d-flex align-items-center justify-content-center middle">
             <Row>
-              <Col className='text-center'>
+              <Col className='typewriter text-center'>
                 <h1><span className='greeting'>Hi!</span> I am Lukasz Jurkiewicz</h1>
                 <h2>Full Stack Developer</h2>
-                <Button href='#about-me'>About me</Button>
               </Col>
             </Row>
         </Container>);
@@ -37,5 +35,11 @@ function Body() {
         </div>
     );
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      document.querySelector(".typewriter h2").style.display = "block";
+    }, 3900); /* Delay should match the duration of h1 animation (3.5 seconds) */
+  });
 
 export default Body;
