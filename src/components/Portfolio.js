7735229ae0e88881a26cda3c933775eef4de2599
Abstynent <lukjur94@gmail.com>
2023-06-22 12:25:57 +0200

    store all projects to be displayed and return component Projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Projects from './Projects';
 
-export default function Portfolio() {
-    return (
-        <div>
-            portfolio page
-        </div>
-    )
-}
+const portfolioProjects = [
+  {
+    title: 'MealPlan',
+    description: 'This group project involves creating an app for searching meal and drink recipes. Users can explore recipes based on categories, ingredients, and regions.',
+    image: '/img/projects/mealplan.png',
+    url: 'https://abstynent.github.io/project1-meal-plan-app/',
+    repo: 'https://github.com/Abstynent/project1-meal-plan-app',
+  }
+];
+
+const Portfolio = () => {
+  return (
+    <div>
+      <h1>My Portfolio</h1>
+      <Projects projects={portfolioProjects} />
+    </div>
+  );
+};
+
+export default Portfolio;
