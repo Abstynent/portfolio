1511150700aff0912c74d46762c4ff940576ebdd
Abstynent <lukjur94@gmail.com>
2023-06-26 00:17:09 +0100

    portfolio bio changed

--- src/pages/partials/AboutSection.js
+++ src/pages/partials/AboutSection.js
@@ -1,35 +1,21 @@
 import React from "react";
 import { Col } from 'react-bootstrap';
+const aboutMeText = `
+Hello, I'm Lukasz!
+I am a passionate full-stack developer with expertise in a range of technologies, including JavaScript, CSS, HTML, React, Express.js, Node.js, MySQL, NoSQL, and the MERN stack. My goal is to create exceptional web applications that deliver seamless user experiences.
+During my full-stack developer course at the University of Birmingham, I have gained hands-on experience working with these technologies. I have built interactive and responsive user interfaces using HTML, CSS, and JavaScript, leveraging the power of React for dynamic front-end development.
+On the back-end, I have a strong foundation in Express.js and Node.js, enabling me to build robust server-side applications. I have used Express.js to create APIs, handle routing, and manage server-side logic. Node.js has allowed me to build scalable and efficient server-side applications, leveraging its event-driven architecture.
+Database management is another area of expertise, as I have worked extensively with both relational databases like MySQL and non-relational databases like NoSQL. I have designed and optimized database schemas, written complex queries, and ensured data integrity.
+I am committed to continuous learning, staying updated with the latest industry trends and best practices. I actively seek new technologies and frameworks to expand my skill set and tackle new challenges.
+Collaboration and problem-solving are fundamental aspects of my approach. I thrive in team environments, leveraging my technical skills to contribute to projects and finding innovative solutions to complex problems.
+Thank you for visiting my portfolio. I am excited about the potential to apply my skills and contribute to impactful projects. If you have any questions or would like to discuss potential collaborations, I would be delighted to connect with you.
 
+`
 export default function AboutSection() {
     return (
         <Col md={9} className='content-col'>
-                    <h1>About me</h1>
-                    <p>I'm Lukasz, a passionate and highly skilled full stack developer. 
-                        My expertise lies in creating innovative and efficient web applications. 
-                        With a comprehensive understanding of both front-end and back-end development, 
-                        I'm proficient in a variety of programming languages, frameworks, and technologies. 
-                        I thrive on solving complex problems and pay meticulous attention to detail when designing
-                        and implementing robust solutions that cater to the needs of clients and end-users. 
-                        Collaborative environments are where I shine, as I possess excellent teamwork skills and effective communication abilities.
-                        I continuously stay up-to-date with the latest industry trends and best practices, 
-                        ensuring that I deliver top-quality results. I'm driven by a commitment to excellence and strive to exceed expectations in all my work.</p>
-                        
-                    <h2>Skills:</h2>
-                        <ul className='skills'>
-                        <li>JavaScript: Proficient in using JavaScript for developing dynamic and interactive web applications.</li>
-                        <li>HTML: Experienced in crafting well-structured and semantic web pages using HTML.</li>
-                        <li>CSS: Skilled in creating visually appealing and responsive designs with CSS.</li>
-                        <li>Express.js: Experienced in building server-side applications using the Express.js framework.</li>
-                        <li>MySQL: Proficient in working with MySQL for structured data storage and retrieval.</li>
-                        <li>NoSQL (MongoDB): Knowledgeable in handling unstructured data using MongoDB as a NoSQL database.</li>
-                        <li>React: Proficient in building dynamic and scalable user interfaces with React.</li>
-                        <li>GraphQL: Experienced in efficient data querying and manipulation using GraphQL.</li>
-                        <li>Node.js: Skilled in server-side development and leveraging JavaScript with Node.js.</li>
-                        <li>RESTful API: Experienced in building RESTful APIs for seamless client-server communication.</li>
-                        <li>Progressive Web Applications (PWA): Skilled in developing web applications that provide native-like experiences to users.</li>
-                        <li>MERN Stack: Proficient in utilizing the MERN stack (MongoDB, Express.js, React, Node.js) for full stack development.</li>
-                        </ul>
-                </Col>
+            <h1>About me</h1>
+            <p style={{ textAlign: 'left' }}>{aboutMeText}</p>
+        </Col>
     );
 };
