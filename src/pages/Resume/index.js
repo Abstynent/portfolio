17ea9b1dfb372ad3b6fab23631480f109c5d0b9f
Abstynent <lukjur94@gmail.com>
2023-06-27 18:31:01 +0100

    resume page added with button to let user download my cv

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Button, Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileArrowDown } from '@fortawesome/free-solid-svg-icons';
 
-export default function Resume() {
-    return (
-        <div>
-            resume page
-        </div>
-    )
-}
+const Resume = () => {
+  const downloadCV = (event) => {
+    event.preventDefault();
+    const cvPath = '/assets/files/resume.pdf';
+    window.open(cvPath, '_blank');
+  };
+
+  return (
+    <Container>
+      <Row className="justify-content-center custom-container">
+        <Col md={3} className='avatar-col'>
+          <img src='/assets/img/resume.png' alt='Avatar' className='avatar-img' />
+        </Col>
+
+        <Col xs={12} sm={6} className="text-center mt-4">
+          <h1 className="mb-4">My resume</h1>
+          <div className="d-flex justify-content-center">
+            <Button variant="link" onClick={downloadCV}>
+              <FontAwesomeIcon icon={faFileArrowDown} size="6x" />
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Resume;
